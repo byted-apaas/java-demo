8c099e76437e2ad01bd66a6e7cfab468ed683775
zhangrongjie.123 <zhangrongjie.123@bytedance.com>
2024-09-18 11:40:41 +0800

    chore: use PostMapping

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,21 +2,22 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/apaas")
 public class TestController {
 
-    @RequestMapping("/test1")
+    @PostMapping("/test1")
     @ResponseBody
     public String testDemo1() {
         return "Hello aPaaS!";
     }
 
-     @RequestMapping("/test2")
-        @ResponseBody
-        public String testDemo2() {
+    @PostMapping("/test2")
+    @ResponseBody
+    public String testDemo2() {
             return "Hello aPaaS!";
         }
 }
